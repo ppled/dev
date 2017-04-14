73ed998497377be2050c84eaae233cd52f80a8ac
Chris Deacy <chrisdothtml@gmail.com>
2017-04-14 16:41:16 -0500

    adding server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('running...')
+// environment vars
+require('dotenv').load()
+
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler (request, reply) {
+    return reply('Petersen Parts dev')
+  }
+})
+
+server.start(err => {
+  if (err) throw err
+
+  console.log('Server running at:', server.info.uri)
+})
