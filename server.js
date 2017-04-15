6784a8a66b5e9dbc8e1bd508df2053a5f4386b78
Chris Deacy <chrisdothtml@gmail.com>
2017-04-14 20:27:03 -0500

    migrating redesign progress to public folder

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 require('dotenv').load()
 
 const express = require('express')
+const { static } = express
+const { join } = require('path')
+
 const app = express()
 const port = process.env.PORT || 1337
 
-app.get('/', (req, res) => {
-  res.send('Petersen Parts dev')
-})
+app.use(static(join(__dirname, 'public')))
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
