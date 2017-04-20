98401740f1582542c1174647efdc51a3bed15dbb
Chris Deacy <chrisdothtml@gmail.com>
2017-04-20 16:00:27 -0500

    restructuring css files to have page-specific builds; adding gulp for dynamic file building and css processing

---
+++ public/redesign/gulpfile.js
@@ -0,0 +1,24 @@
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
+const stylus = require('gulp-stylus')
+
+function buildCSS () {
+  return gulp.src('src/*.styl')
+    .pipe(sourcemaps.init())
+      .pipe(stylus())
+      .pipe(autoprefixer())
+      .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets'))
+}
+
+function watch () {
+  if (process.argv[3] === '--watch') {
+    gulp.watch('src/**/*.styl', ['build:css'])
+  }
+}
+
+gulp.task('build:css', buildCSS)
+gulp.task('build', ['build:css'], watch)
