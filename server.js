eec3529be4ac2e7cd47e0bbb4ba8bde01a9b7dbb
Chris Deacy <chrisdothtml@gmail.com>
2017-04-20 19:52:13 -0500

    adding server auth for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // environment vars
 require('dotenv').load()
 
+const auth = require('express-basic-auth')
 const express = require('express')
 const { static } = express
 const { join } = require('path')
@@ -8,8 +9,32 @@ const { join } = require('path')
 const app = express()
 const port = process.env.PORT || 1337
 
-app.use(static(join(__dirname, 'public')))
+function getUsers () {
+  // users in `USER::PASS;` format
+  const USERS = process.env.USERS || ''
+  const result = {}
 
+  USERS.split(';')
+    // remove falsy
+    .filter(item => item)
+    .forEach(pair => {
+      const [user, pass] = pair.split('::')
+      result[user] = pass
+    })
+
+  return result
+}
+
+// authentication
+if (process.env.NODE_ENV === 'production') {
+  app.use(auth({
+    challenge: true,
+    realm: 'ppled-dev-1337',
+    users: getUsers()
+  }))
+}
+
+app.use(static(join(__dirname, 'public')))
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
