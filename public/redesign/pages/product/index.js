3ee91a583612f0aee29c5dba17d9a87ba47d42e5
Chris Deacy <chrisdothtml@gmail.com>
2017-09-24 19:47:40 -0500

    adding media-gallery and product-info to product page; finishing main product section

--- public/redesign/pages/product/index.js
+++ public/redesign/pages/product/index.js
@@ -1,5 +1,10 @@
 import RateStars from '../../lib/components/rate-stars/index.js'
 
+$('#product-main-gallery [data-fancybox]').fancybox({
+  loop: true,
+  toolbar: false
+})
+
 new RateStars(
   document.getElementById('product-rate-stars')
 )
