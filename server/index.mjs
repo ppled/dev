d250d833893d1c470c17d85cc64797f1abd0e2d2
Chris Deacy <chrisdothtml@gmail.com>
2017-10-05 20:35:28 -0500

    adding sass middleware for eventual conversion

--- server/index.mjs
+++ server/index.mjs
@@ -3,6 +3,7 @@ import env from './env.mjs'
 import Koa from 'koa'
 import mwAuth from './middleware/auth.mjs'
 import mwRollup from './middleware/rollup.mjs'
+import mwSass from './middleware/sass.mjs'
 import mwStylus from './middleware/stylus.mjs'
 import serve from 'koa-static'
 
@@ -15,6 +16,7 @@ if (NODE_ENV === 'production') {
 }
 
 server.use(mwRollup(PUBLIC_PATH))
+server.use(mwSass(PUBLIC_PATH))
 server.use(mwStylus(PUBLIC_PATH))
 server.use(addTrailingSlashes())
 server.use(serve(PUBLIC_PATH))
